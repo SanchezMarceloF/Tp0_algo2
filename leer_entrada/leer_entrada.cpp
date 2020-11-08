0d75879bb9e9df7e090e8d73fa9afe14830bd816
Sanchez Marcelo <sanchez.marcelof1986@gmail.com>
2020-11-08 12:33:55 -0300

    Se sube archivo main.cc modificado con argumento difficulty
    Se borro funcion multiply y adecuaron los mensajes para la opcion
    difficulty.
    Complila OK con Makefile.

--- leer_entrada/leer_entrada.cpp
+++
@@ -1,8 +0,0 @@
-//leer_entrada.cpp
-//este modulo toma las transacciones que se pasan por linea de comando
-//y devuelve una cadena de caracteres al programa principal
-//se utilizan los archivos cmdline (main.cc cmdline.cc cmdline.h) dados en la teorica.
-
-#include <iostream>
-
-
