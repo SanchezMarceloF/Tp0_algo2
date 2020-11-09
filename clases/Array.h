ef6385a5ef632293589a9bf469f3bf059d7be4b7
Sanchez Marcelo <sanchez.marcelof1986@gmail.com>
2020-11-09 03:24:01 -0300

    Se modificó y probó una primera vesión de Array.h
    Con el archivo prueba0.cpp se implementaron Arrays de la estructura
    "output_t". Se probó con exito los constructores y metodos:
    o constructor sin argumento
    o constructor con tamaño
    o destructor
    o setArray con argumentos <T> e indice
    o operator[]
    para correr usar make prueba0.

---
+++ clases/Array.h
@@ -0,0 +1,129 @@
+#include<iostream>
+#ifndef ARRAYTEMP_H
+#define ARRAYTEMP_H
+
+template <class T>
+class Array{
+public:
+	//Constructores
+	Array();
+	Array(const Array<T> &);
+	Array(size_t);
+	//Destructor
+	~Array();
+	
+	//seters
+	Array<T>& setArray(const T&, int);
+		
+	//getters
+	int getSize();
+		
+	//otros metodos	
+	Array<T> &operator=(const Array<T> &);
+	bool operator==(const Array<T> &);
+	T &operator[](int);
+	//void emitir();
+
+private:
+	int size;
+	T *ptr;
+};
+
+//constructor sin argumentos
+template <class T>
+Array<T>::Array(){
+	ptr=new T[10];
+	size=10;
+}
+
+//constructor con parametro tamaño
+template <class T>
+Array<T>::Array(size_t n){
+	std::cout<<"constructor Array con argumento tamaño" << std::endl;
+	size=n;
+	ptr=new T[size];
+}
+
+//constructor con Array como parametro
+template <class T>
+Array<T>::Array(const Array<T> &init){
+	std::cout<<"constructor Array con argumento Array" << std::endl;
+	size = init.size;
+	ptr = new T[size];
+	for(int i=0; i<size; i++)
+		ptr[i] = init.ptr[i];
+}
+
+//Destructor
+template <class T>
+Array<T>::~Array(){
+	std::cout<<"destructor Array" << std::endl;
+	if(ptr)
+		delete []ptr;
+}
+
+//setters
+//carga de un objeto en 
+template <class T>
+Array<T>& Array<T>::setArray(const T& aux, int inx){
+	if (size < inx-1){
+		std::cout<<"no se puede cargar valor"<<std::endl;
+		return *this;
+	}
+	else{
+		ptr[inx] = aux;
+		return *this;
+	}
+}
+
+
+//getters
+//metodo para obtener parametro size
+template <class T>
+int Array<T>::getSize(){
+	return size;
+}
+
+//otros metodos
+template <class T>
+Array<T>& Array<T>::operator=( const Array<T> &rigth ){
+	if ( &rigth != this ){
+		if ( size != rigth.size ){
+			T *aux;
+			aux = new T[ rigth.size ];
+			delete [] ptr;
+			size = rigth.size ;
+			ptr = aux;
+			for (int i = 0; i < size; i++)
+				{ptr[ i ] = rigth.ptr[ i ];}
+			return *this;
+		}
+		else{
+		 	for ( int i = 0; i < size; i++ )
+				ptr[ i ] = rigth.ptr[ i ];
+		return *this;
+		}
+	}
+	return *this;
+}
+
+template <class T>
+bool Array<T>::operator==( const Array<T> &rigth ){
+	if ( size != rigth.size )
+		return false;
+	else
+		for ( int i = 0; i < size; i++ )
+			if ( ptr[i] != rigth.ptr[i] )
+				return false;
+	else
+		return true;
+}
+
+template <class T>
+T & Array<T>::operator[]( int subscript ){
+	return ptr[ subscript ];
+}
+
+#endif 
+
+
---
+++ clases/prueba0.cpp
@@ -0,0 +1,31 @@
+//prueba del Template Array con una 
+//estructura output_t
+
+#include <iostream>
+#include <string>
+#include "Array.h"
+
+using namespace std;
+
+typedef struct{
+        float value;
+        std::string addr; //dirección destino de los fondos, 64 bytes fijos (es un hash)
+} output_t;
+
+int main() {
+	cout<<"estamos en main de pruebao.cpp"<< endl;
+	Array<output_t> a, b(5);
+	output_t c, z;
+
+	c.value = 324.56;
+	c.addr = "64a4171b3c45d34131f16a10405003b3515f13131355133057";
+	a.setArray(c,3); //copio c en la pos 3 del Array
+	z = a[3];
+	cout<<"z.value = "<<z.value<<endl;
+	cout<<"z.addr = "<<z.addr<<endl;
+	//Array d(c,3w;
+		
+return 0;
+}
+
+
