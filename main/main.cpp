85b8993a6b6d9722bd1b2b704efc0158115aa3c6
Sanchez Marcelo <sanchez.marcelof1986@gmail.com>
2020-11-19 06:19:00 -0300

    Se agrega main con funcion leer
    o el hash de las transacciones funciona
     :: Falta ::
    o el hash del header
    o acomodar/modificar funcion leer en main
    o agregar chequeos a la entrada
    o crear test files

---
+++ main/main.cpp
@@ -0,0 +1,250 @@
+// utiliza la Clase cmdline parar parsear los argumentos pasados por 
+// linea de comnados. El bloque genera un puntero a un stream para 
+// procesar los datos del programa desde el main.
+//Se verifica que la dificultad pasada por argumento sea menor a 6.
+//
+// $Id: main.cc,v 1.5 2012/09/15 12:23:57 lesanti Exp $
+
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <cstdlib>
+
+#include "cmdline.h"
+#include "sha256.h"
+#include "txns_lecture.h"
+#include "block.h"
+
+using namespace std;
+
+
+static void opt_input(string const &);
+static void opt_output(string const &);
+static void opt_difficulty(string const &);
+static void opt_help(string const &);
+
+// Tabla de opciones de l�nea de comando. El formato de la tabla
+// consta de un elemento por cada opci�n a definir. A su vez, en
+// cada entrada de la tabla tendremos:
+//
+// o La primera columna indica si la opci�n lleva (1) o no (0) un
+//   argumento adicional.
+//
+// o La segunda columna representa el nombre corto de la opci�n.
+//
+// o Similarmente, la tercera columna determina el nombre largo.
+//
+// o La cuarta columna contiene el valor por defecto a asignarle
+//   a esta opci�n en caso que no est� expl�citamente presente
+//   en la l�nea de comandos del programa. Si la opci�n no tiene
+//   argumento (primera columna nula), todo esto no tiene efecto.
+//
+// o La quinta columna apunta al m�todo de parseo de la opci�n,
+//   cuyo prototipo debe ser siempre void (*m)(string const &arg);
+//
+// o La �ltima columna sirve para especificar el comportamiento a
+//   adoptar en el momento de procesar esta opci�n: cuando la
+//   opci�n es obligatoria, deber� activarse OPT_MANDATORY.
+//
+// Adem�s, la �ltima entrada de la tabla debe contener todos sus
+// elementos nulos, para indicar el final de la misma.
+//
+static option_t options[] = {
+	{1, "i", "input", "-", opt_input, OPT_DEFAULT},
+	{1, "o", "output", "-", opt_output, OPT_DEFAULT},
+	//{1, "f", "factor", NULL, opt_factor, OPT_MANDATORY},
+	{1, "d", "difficulty", NULL, opt_difficulty, OPT_MANDATORY},
+	{0, "h", "help", NULL, opt_help, OPT_DEFAULT},
+	{0, },
+};
+//static int factor;
+static int difficulty;
+static istream *iss = 0;
+static ostream *oss = 0;
+static fstream ifs;
+static fstream ofs;
+static string input;
+static string output;
+
+static void
+opt_input(string const &arg)
+{
+	// Si el nombre del archivos es "-", usaremos la entrada
+	// est�ndar. De lo contrario, abrimos un archivo en modo
+	// de lectura.
+	//
+	if (arg == "-") {
+		iss = &cin;
+	} else {
+		ifs.open(arg.c_str(), ios::in);
+		iss = &ifs;
+	}
+
+	// Verificamos que el stream este OK.
+	//
+	if (!iss->good()) {
+		cerr << "cannot open "
+		     << arg
+		     << "."
+		     << endl;
+		exit(1);
+	}
+}
+
+static void
+opt_output(string const &arg)
+{
+	// Si el nombre del archivos es "-", usaremos la salida
+	// est�ndar. De lo contrario, abrimos un archivo en modo
+	// de escritura.
+	//
+	if (arg == "-") {
+		oss = &cout;
+	} else {
+		ofs.open(arg.c_str(), ios::out);
+		oss = &ofs;
+	}
+
+	// Verificamos que el stream este OK.
+	//
+	if (!oss->good()) {
+		cerr << "cannot open "
+		     << arg
+		     << "."
+		     << endl;
+		exit(1);
+	}
+}
+
+
+static void
+opt_difficulty(string const &arg)
+{
+	istringstream iss(arg);
+	
+	// Intentamos extraer la dificultad de la l�nea de comandos.
+	// Para detectar argumentos que �nicamente consistan de 
+	// n�meros enteros, vamos a verificar que EOF llegue justo
+	// despu�s de la lectura exitosa del escalar.
+	//
+	if (!(iss >> difficulty)
+	    || !iss.eof()) {
+		cerr << "non-integer difficulty: "
+		     << arg
+		     << "."
+		     << endl;
+		exit(1);
+	}
+
+	if (iss.bad()) {
+		cerr << "cannot read integer difficulty."
+		     << endl;
+		exit(1);
+	}
+}
+
+static void
+opt_help(string const &arg)
+{
+	//cout << "cmdline -f factor [-i file] [-o file]"
+	cout << "cmdline -d difficulty [-i file] [-o file]"
+	     << endl;
+	exit(0);}
+
+void
+leer(istream *is, ostream *os)
+{
+	unsigned n_in, n_out;
+	Block block_a;
+	Transaction txns_a;
+	input_t in_j;
+	output_t out_m;
+	
+	//chequeo que la primera linea contenga un unsigned
+	if(!(*is >> n_in)) 
+		cout<<"ERROR EN TXNS IN"<<endl;
+	else{
+		for(unsigned i=0; i<n_in ; i++){
+			*is >> in_j.tx_id; 		
+			*is >> in_j.idx;
+			*is >> in_j.addr;
+			txns_a.setTx(in_j);
+		}
+		if(!(*is >> n_out)) 
+			cout<<"ERROR TXNS OUT"<<endl;
+		else{
+			for(unsigned i=0; i<n_out ; i++){
+				*is >> out_m.value; 		
+				*is >> out_m.addr;
+				txns_a.setTx(out_m);
+			}
+		}		
+	}
+	block_a.setTxns(txns_a);
+	
+	*os<<":: Transacciones en el Bloque :: \n\n";
+	input = block_a.getTxns();
+	output = block_a.getTxnsHash();
+	output = sha256(sha256(input));
+	*os << input <<endl;
+	*os <<"Hash: "<<output<<endl; 
+	
+	//while (*is >> num) {
+	//	*os << num * difficulty
+	//	    << "\n";
+	//}
+	if (os->bad()) {
+		cerr << "cannot write to output stream."
+		     << endl;
+		exit(1);
+	}
+	if (is->bad()) {
+		cerr << "cannot read from input stream."
+		     << endl;
+		exit(1);
+	}
+	if (!is->eof()) {
+		cerr << "cannot find EOF on input stream."
+		     << endl;
+		exit(1);
+	}
+}
+
+int
+main(int argc, char * const argv[])
+{
+	cmdline cmdl(options); //cmdline es una clase definida en cmdline.h
+	cmdl.parse(argc, argv);
+	
+	leer(iss, oss);
+	
+
+}
+
+//static void
+//opt_factor(string const &arg)
+//{
+//	istringstream iss(arg);
+//	
+//	// Intentamos extraer el factor de la l�nea de comandos.
+//	// Para detectar argumentos que �nicamente consistan de 
+//	// n�meros enteros, vamos a verificar que EOF llegue justo
+//	// despu�s de la lectura exitosa del escalar.
+//	//
+//	if (!(iss >> factor)
+//	    || !iss.eof()) {
+//		cerr << "non-integer factor: "
+//		     << arg
+//		     << "."
+//		     << endl;
+//		exit(1);
+//	}
+//
+//	if (iss.bad()) {
+//		cerr << "cannot read integer factor."
+//		     << endl;
+//		exit(1);
+//	}
+//}
+
